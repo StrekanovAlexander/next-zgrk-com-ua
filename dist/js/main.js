1f7856ca41abf89d980b09fb23f0e252cfd53f20
StrekanovAlexander <strekanov.alexander@gmail.com>
2020-04-24 12:53:17 +0300

    rebuilded: with navElemSeleced

--- dist/js/main.js
+++ dist/js/main.js
@@ -9,7 +9,6 @@ navBtn.addEventListener('click', function() {
 }); 
 
 window.addEventListener('resize', function() {
-  console.log(window.innerWidth);
   if (window.innerWidth > 1024) {
     if (navList.classList.contains('nav__list_mobile')) {
       navList.classList.remove('nav__list_mobile');
@@ -27,5 +26,10 @@ function display(elem) {
   elem.style.display = elem.style.display === 'block' ? 'none' : 'block';
 }
 
-// Scenes
-
+navElems = navList.querySelectorAll('li > a');
+for (var i = 0; i < navElems.length; i +=1) {
+  navElems[i].addEventListener('click', function(ev) {
+    document.querySelector('.nav__list > li > a.selected').classList.remove('selected');
+    ev.target.classList.add('selected');
+  });
+}
