3a6c9f250394ffd870af5f6fa7bc7e5c57441d8e
StrekanovAlexander <strekanov.alexander@gmail.com>
2020-04-24 08:54:19 +0300

    rebuilded: with nav-mobile functional

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,7 +2,27 @@ var navBtn = document.querySelector('.nav__btn');
 var navList = document.querySelector('.nav__list');
 
 navBtn.addEventListener('click', function() {
-  var navMobile = document.querySelector('.nav__list_mobile');
-  var opacity = navMobile.style.opacity; 
-  opacity = opacity === '1' ? '0' : '1';
+  if (!navList.classList.contains('nav__list_mobile')) {
+    navList.classList.add('nav__list_mobile');
+  }
+  display(navList);
 }); 
+
+window.addEventListener('resize', function() {
+  console.log(window.innerWidth);
+  if (window.innerWidth > 1024) {
+    if (navList.classList.contains('nav__list_mobile')) {
+      navList.classList.remove('nav__list_mobile');
+      navList.style.display = 'flex';
+    }
+  } else {
+    if (!navList.classList.contains('nav__list_mobile')) {
+      navList.classList.add('nav__list_mobile');
+      navList.style.display = 'none';
+    }
+  }
+});
+
+function display(elem) {
+  elem.style.display = elem.style.display === 'block' ? 'none' : 'block';
+}
